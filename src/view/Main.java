f24288b1ed82217e84b15208de0e740b8e656ca5
Damien <damien.quagliaro@gmail.com>
2019-01-07 16:24:51 +0100

    Deleting useless lines of code in Main

--- src/view/Main.java
+++ src/view/Main.java
@@ -12,18 +12,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Signal Sig = new Signal();
-			SignalTools SigT = new SignalTools();
 			Sig.setFreq(880);
-			System.out.println("Freq = " + Sig.getFreq() + " Hz");
 			Sig.setFe(44100);
-			System.out.println("Fe = " + Sig.getFe() + " Hz");
 			Sig.setNbEch(100);
-			System.out.println("NbEch = " + Sig.getNbEch());
-			// Sig.drawSignal(SignalType.SINE) ;
 			ArrayList<Double> L = Sig.getSig();
-			System.out.println(L);
-			System.out.println("Mean = " + SigT.computeTNS(Sig, "mean"));
-			// Screen S = new Screen(primaryStage, L) ;
 			ControlPane C = new ControlPane(primaryStage, Sig);
 		} catch (Exception e) {
 			e.printStackTrace();
